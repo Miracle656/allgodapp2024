21ee30b4e20e97bd9673f2a7f2b0c4c3407a91dd
Ebube <web3sultan@localhost.localdomain>
2024-10-31 06:16:34 +0100

    didcontract

--- projects/algo_dapp-contracts/smart_contracts/dapp_contract/contract.py
+++ projects/algo_dapp-contracts/smart_contracts/dapp_contract/contract.py
@@ -2,7 +2,50 @@ from algopy import ARC4Contract, String
 from algopy.arc4 import abimethod
 
 
+# class DappContract(ARC4Contract):
+#     @abimethod()
+#     def hello(self, name: String) -> String:
+#         return "Hello, " + name
+
+
 class DappContract(ARC4Contract):
+
+    did: String
+    public_key: String
+    
+    #create the app
+    @abimethod(allow_actions=["NoOp"], create="require")
+    def creatApplication(self, did: String, public_key: String) -> None:
+        self.did = did
+        self.public_key = public_key
+    
+
     @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def register_identity(self, did: String, public_key: String) -> String:
+        key_val: dict[String, String] = {
+            String('myexample.algo'): String('algo1234567890')
+        }
+        if len(key_val) != 0:
+            return "DID: " + did + " is already registered"
+
+        key_val[public_key] = did
+        return "DID successfully registerd: " + did
+
+# class DappContract(ARC4Contract):
+
+#     global_state = GlobalState(str)
+
+#     def __init__(self) -> None:
+#         super().__init__()
+
+#         self.did_key = self.global_state.key("did_key")
+
+#         self.public_key = self.global_state.key("public_key")
+
+#     @abimethod()
+#     def register_identity(self, did: String, public_key: String) -> str:
+#         if self.did_key.has(did):
+#             return "DID already registered"
+
+#         self.did_key.set(did, public_key)
+#         return "Identity registered with DID:" + str(did) +  "and public key:" + str(public_key)
